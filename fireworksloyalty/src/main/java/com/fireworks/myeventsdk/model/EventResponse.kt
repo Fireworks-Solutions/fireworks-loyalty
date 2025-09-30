700bcb6f123b4c9c92e94c229263d59a52437555
Priyasprojects <priyankafireworks@gmail.com>
2025-09-30 16:31:06 +0800

    NewKeyAdded inside the EventList

--- fireworksloyalty/src/main/java/com/fireworks/myeventsdk/model/EventResponse.kt
+++ fireworksloyalty/src/main/java/com/fireworks/myeventsdk/model/EventResponse.kt
@@ -3,5 +3,6 @@ package com.fireworks.myeventsdk.model
 data class EventResponse(
     val status: String,
     val message: String?,
+    val Featuredevents: List<Event>,
     val events: List<Event>
 )
