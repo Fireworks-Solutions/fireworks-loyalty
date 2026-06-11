e48217f430bc80ec99e5ed25dc34cb8ff1c4cfa1
Priyasprojects <priyankafireworks@gmail.com>
2026-06-11 13:26:49 +0800

    updated title for event

--- fireworksloyalty/src/main/java/com/fireworks/myeventsdk/model/EventResponse.kt
+++ fireworksloyalty/src/main/java/com/fireworks/myeventsdk/model/EventResponse.kt
@@ -3,6 +3,8 @@ package com.fireworks.myeventsdk.model
 data class EventResponse(
     val status: String,
     val message: String?,
+    val title_1: String?,
+    val title_2: String?,
     val featured_events: List<Event>,
     val events: List<Event>
 )
